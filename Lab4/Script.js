be6c6f8eabe30729d9d258e9655e2419d3bedcab
Khilchuk-Artem <artem.khilchuk@gmail.com>
2023-09-29 19:48:54 +0300

    Minor fixes

--- Lab4/Script.js
+++ Lab4/Script.js
@@ -1,120 +1,109 @@
-/*const BLUE_ELEM_CLASSNAME = "blue-elem";
-const PINK_ELEM_CLASSNAME = "pink-elem";
+//subtask 1
 
 const addTextClickListeners = () => {
   document.getElementById("fifth-one").addEventListener("click", (e) => {
     const elem = document.getElementById("fifth-one");
-    handleClick(elem, BLUE_ELEM_CLASSNAME);
-  });
-
-  document
-    .querySelector("#sixth-elem ~ ul")
-    .firstElementChild.addEventListener("click", (e) => {
-      const elem = document.querySelector("#sixth-elem ~ ul").firstElementChild;
-      handleClick(elem, PINK_ELEM_CLASSNAME);
-    });
-};
-
-const handleClick = (elem, className) => {
-  if (elem.classList.contains(className)) {
-    elem.classList.remove(className);
-    return;
-  }
-
-  elem.classList.add(className);
-};
-
-const addBtnClickListeners = () => {
-  const imgElemId = "city-img";
-  const imgWrapperElemId = "img-wrapper";
-  const containerId = "imgs-container";
-
-  const container = document.getElementById(containerId);
-
-  const zoomDelta = 0.2;
-
-  const getLastImgWrInContainer = () => {
-    const imgs = document.querySelectorAll(
-      `#${containerId} #${imgWrapperElemId}`
-    );
-
-    if (imgs.length === 0) {
-      return null;
-    }
-
-    return imgs[imgs.length - 1];
-  };
-
-  document.getElementById("add-btn").addEventListener("click", (e) => {
-    const imgElem = document.createElement("img");
-    imgElem.id = imgElemId;
-    imgElem.src = "./lviv.jpeg";
-
-    const wrappedImg = document.createElement("div");
-    wrappedImg.id = imgWrapperElemId;
-    wrappedImg.appendChild(imgElem);
-
-    container.appendChild(wrappedImg);
-  });
-
-  document.getElementById("delete-btn").addEventListener("click", (e) => {
-    const lastImg = getLastImgWrInContainer();
-
-    if (lastImg === null) {
+    if(elem.style.cssText!=""){
+      elem.style.cssText = "";
       return;
     }
-
-    container.removeChild(lastImg);
+    elem.style.backgroundColor = "blue";
+    elem.style.color = "white";
   });
 
-  document.getElementById("zoom-in-btn").addEventListener("click", (e) => {
-    zoomLastImgElem(zoomDelta);
-  });
-
-  document.getElementById("zoom-out-btn").addEventListener("click", (e) => {
-    zoomLastImgElem(-zoomDelta);
-  });
-
-  const zoomLastImgElem = (zoomDifference) => {
-    let targetImgWr = getLastImgWrInContainer();
-    if (targetImgWr === null) {
-      targetImgWr = document.getElementById("link-img-wrapper");
-    }
-
-    const imgElem = targetImgWr.querySelector("#city-img");
-    const originalStyle = window.getComputedStyle(imgElem);
-    const transform = originalStyle.getPropertyValue("transform");
-
-    if (transform === "none") {
-      imgElem.style.transform = "matrix(1.2, 0, 0, 1.2, 0, 0)";
+/*document.getElementById("sixth-one").addEventListener("click", (e) => {
+    const elem = document.getElementById("sixth-one");
+    if(elem.style.cssText!=""){
+      elem.style.cssText = "";
       return;
     }
+    elem.style.backgroundColor = "green";
+    elem.style.color = "yellow";
+  });*/
 
-    let paramsStr = transform.match("[\\d\\., ]+")[0];
-    let params = paramsStr.split(", ");
-
-    params[0] = parseFloat(params[0]) + zoomDifference;
-    params[3] = parseFloat(params[3]) + zoomDifference;
-
-    imgElem.style.transform = `matrix(${params.join(", ")})`;
-  };
+  document.querySelector("#sixth-one").addEventListener("click", (e) => {
+const elem = document.querySelector("#sixth-one");
+if (elem.style.cssText != "") {
+  elem.style.cssText = "";
+  return;
+}
+elem.style.backgroundColor = "green";
+elem.style.color = "yellow";
+});
 };
-
 addTextClickListeners();
-addBtnClickListeners();*/
+
+//subtask 2
+var imgCounts=1;
 
 
-const addTextClickListeners = () => {
-    document.getElementById("fifth-one").addEventListener("click", (e) => {
-      const elem = document.getElementById("fifth-one");
-      element.style.backgroundColor = "blue";
-    });
-  
-    document
-      .querySelector("#sixth-elem ~ ul")
-      .firstElementChild.addEventListener("click", (e) => {
-        const elem = document.querySelector("#sixth-elem ~ ul").firstElementChild;
-        handleClick(elem, PINK_ELEM_CLASSNAME);
-      });
-  };
-addTextClickListeners();
+
+const addButtonListeners=()=>{
+document.getElementById("add-btn").addEventListener("click",()=>{
+  document.getElementById("images").innerHTML+='<a href="https://slavuta-mvk.gov.ua/" id="img-wrapper-'+imgCounts+'">' +
+  ' <div style="width:500px;overflow:hidden"> <img src="https://upload.wikimedia.org/wikipedia/commons/b/bf/%D0%A6%D0%B5%D0%BD%D1%82%D1%80-%D0%BC%D1%96%D1%81%D1%82%D0%B0-%D0%A1%D0%BB%D0%B0%D0%B2%D1%83%D1%82%D0%B0.jpg" style="width: 500px;;transform:scale(1)" id="img-so-big-'+imgCounts+'"/><br/></div>' +
+  '</a>';
+  imgCounts++;
+});
+
+document.getElementById("delete-btn").addEventListener("click",()=>{
+  document.getElementById('img-wrapper-'+(imgCounts-1)).remove();
+  imgCounts--;
+});
+document.getElementById("zoom-in-btn").addEventListener("click",()=>{
+  console.log('img-so-big-'+(imgCounts-1));
+  var img = document.getElementById('img-so-big-'+(imgCounts-1));
+  console.log(img);
+  // Select the HTML element you want to get the old scale value from
+const elementToInspect = img; // Replace with your element's ID
+
+// Get the computed style of the element
+const computedStyle = window.getComputedStyle(elementToInspect);
+  console.log(computedStyle);
+
+// Extract the scale value from the transform property
+const transform = computedStyle.getPropertyValue("transform");
+  console.log(transform);
+
+// Extract the scale value using regular expression (if it exists)
+
+
+
+let paramsStr = transform.match("[\\d\\., ]+")[0];
+let params = paramsStr.split(", ");
+
+params[0] = parseFloat(params[0]) + 0.25;
+params[3] = parseFloat(params[3]) + 0.25;
+
+img.style.transform = `matrix(${params.join(", ")})`;
+});
+document.getElementById("zoom-out-btn").addEventListener("click",()=>{
+  console.log('img-so-big-'+(imgCounts-1));
+  var img = document.getElementById('img-so-big-'+(imgCounts-1));
+  console.log(img);
+  // Select the HTML element you want to get the old scale value from
+const elementToInspect = img; // Replace with your element's ID
+
+// Get the computed style of the element
+const computedStyle = window.getComputedStyle(elementToInspect);
+  console.log(computedStyle);
+
+// Extract the scale value from the transform property
+const transform = computedStyle.getPropertyValue("transform");
+  console.log(transform);
+
+// Extract the scale value using regular expression (if it exists)
+
+
+
+let paramsStr = transform.match("[\\d\\., ]+")[0];
+let params = paramsStr.split(", ");
+
+params[0] = parseFloat(params[0]) - 0.25;
+params[3] = parseFloat(params[3]) - 0.25;
+
+img.style.transform = `matrix(${params.join(", ")})`;
+});
+}
+
+addButtonListeners();
