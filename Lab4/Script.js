37addfa0fa67f3b10872c8cb206fba2f62ffa264
Khilchuk-Artem <artem.khilchuk@gmail.com>
2023-10-09 16:39:49 +0300

    Slight refactoring plus renamed report

--- Lab4/Script.js
+++ Lab4/Script.js
@@ -12,67 +12,66 @@ const addTextClickListeners = () => {
   });
 
   document.querySelector("#sixth-one").addEventListener("click", (e) => {
-const elem = document.querySelector("#sixth-one");
-if (elem.style.cssText != "") {
-  elem.style.cssText = "";
-  return;
-}
-elem.style.backgroundColor = "green";
-elem.style.color = "yellow";
-});
+    const elem = document.querySelector("#sixth-one");
+    if (elem.style.cssText != "") {
+      elem.style.cssText = "";
+      return;
+    }
+    elem.style.backgroundColor = "green";
+    elem.style.color = "yellow";
+  });
 };
 addTextClickListeners();
 
 //subtask 2
 var imgCounts=1;
 
-
-
 const addButtonListeners=()=>{
-document.getElementById("add-btn").addEventListener("click",()=>{
-  document.getElementById("images").innerHTML+='<a href="https://slavuta-mvk.gov.ua/" id="img-wrapper-'+imgCounts+'">' +
-  ' <div style="width:500px;overflow:hidden"> <img src="https://upload.wikimedia.org/wikipedia/commons/b/bf/%D0%A6%D0%B5%D0%BD%D1%82%D1%80-%D0%BC%D1%96%D1%81%D1%82%D0%B0-%D0%A1%D0%BB%D0%B0%D0%B2%D1%83%D1%82%D0%B0.jpg" style="width: 500px;;transform:scale(1)" id="img-so-big-'+imgCounts+'"/><br/></div>' +
-  '</a>';
-  imgCounts++;
-});
+  document.getElementById("add-btn").addEventListener("click",()=>{
+    document.getElementById("images").innerHTML+=
+    `<a href="https://slavuta-mvk.gov.ua/" id="img-wrapper-${imgCounts}">` +
+    ' <div style="width:500px;overflow:hidden"> <img src='+
+    '"https://upload.wikimedia.org/wikipedia/commons/b/bf/%D0%A6%D0%B5%D0%BD%D1%82%D1%80-%D0%BC%D1%96%D1%81%D1%82%D0%B0-%D0%A1%D0%BB%D0%B0%D0%B2%D1%83%D1%82%D0%B0.jpg"'+ 
+    `style="width: 500px;;transform:scale(1)" id="img-so-big-${imgCounts}"/><br/></div></a>`;
+    imgCounts++;
+  });
 
-document.getElementById("delete-btn").addEventListener("click",()=>{
-  document.getElementById('img-wrapper-'+(imgCounts-1)).remove();
-  imgCounts--;
-});
-document.getElementById("zoom-in-btn").addEventListener("click",()=>{
-var img = document.getElementById('img-so-big-'+(imgCounts-1));
+  document.getElementById("delete-btn").addEventListener("click",()=>{
+    document.getElementById('img-wrapper-'+(imgCounts-1)).remove();
+    imgCounts--;
+  });
 
-const elementToInspect = img; 
+  document.getElementById("zoom-in-btn").addEventListener("click",()=>{
+    var img = document.getElementById('img-so-big-'+(imgCounts-1));
 
-const computedStyle = window.getComputedStyle(elementToInspect);
+    const elementToInspect = img; 
 
-  const transform = computedStyle.getPropertyValue("transform");
+    const computedStyle = window.getComputedStyle(elementToInspect);
 
-let paramsStr = transform.match("[\\d\\., ]+")[0];
-let params = paramsStr.split(", ");
+    const transform = computedStyle.getPropertyValue("transform");
 
-params[0] = parseFloat(params[0]) + 0.25;
-params[3] = parseFloat(params[3]) + 0.25;
+    let paramsStr = transform.match("[\\d\\., ]+")[0];
+    let params = paramsStr.split(", ");
 
-img.style.transform = `matrix(${params.join(", ")})`;
-});
-document.getElementById("zoom-out-btn").addEventListener("click",()=>{
-  console.log('img-so-big-'+(imgCounts-1));
-  var img = document.getElementById('img-so-big-'+(imgCounts-1));
+    params[0] = parseFloat(params[0]) + 0.25;
+    params[3] = parseFloat(params[3]) + 0.25;
 
-  const elementToInspect = img; 
+    img.style.transform = `matrix(${params.join(", ")})`;
+  });
+  document.getElementById("zoom-out-btn").addEventListener("click",()=>{
+    
+    var img = document.getElementById('img-so-big-'+(imgCounts-1));
 
-const transform = computedStyle.getPropertyValue("transform");
+    const transform = window.getComputedStyle(img).getPropertyValue("transform");
 
-let paramsStr = transform.match("[\\d\\., ]+")[0];
-let params = paramsStr.split(", ");
+    let paramsStr = transform.match("[\\d\\., ]+")[0];
+    let params = paramsStr.split(", ");
 
-params[0] = Math.max(parseFloat(params[0]) - 0.25,0.1);
-params[3] = Math.max(parseFloat(params[3]) - 0.25,0.1);
+    params[0] = Math.max(parseFloat(params[0]) - 0.25,0.1);
+    params[3] = Math.max(parseFloat(params[3]) - 0.25,0.1);
 
-img.style.transform = `matrix(${params.join(", ")})`;
-});
+    img.style.transform = `matrix(${params.join(", ")})`;
+  });
 }
 
 addButtonListeners();
