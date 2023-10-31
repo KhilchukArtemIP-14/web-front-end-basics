bfa72037d677db8033ce40ceb30561fdc4591732
Khilchuk-Artem <artem.khilchuk@gmail.com>
2023-10-31 19:56:57 +0200

    Trynna deploy it in this repo :'(

--- Lab7/lab-7/src/main.jsx
+++ Lab7/lab-7/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router> 
+    <Router basename="/web-front-end-basics/Lab7/"> 
       <App />
     </Router>
   </React.StrictMode>,
